61be7b9f90484539ca8d002418f500cb511cbaaf
H1ddenShadow <156337267+H1ddenShadow@users.noreply.github.com>
2024-10-02 20:34:10 +0200

    FAQ.py

--- Scripts/FAQ.py
+++ Scripts/FAQ.py
@@ -48,16 +48,22 @@ FAQ Section:
   - The times present in the 'Schedule.py' script are merely default vaules and adjusting the values from the script itself, after already having run said script, will lead to the changes not being reflected unless the user: deletes the created 'scheduled_runs.json' file and stops the script. Users who wish to make changes to the times are suggested to either do so before running the respective script, initially (the first time) or by running the 'Stop.py' script and then editing the times in the 'scheduled_runs.json' file, which can be found in the 'Scheduled Runs' folder.
 
 7. How do I run or start the script?
-  - Users are provided with two options in terms of running the script. They could either run the script once: 'Run.py' or keep it running continuously: 'Start.py'; in the event that they wish to stop running the script, they may then run the following script: 'Stop.py' and should they wish to disable the script completetly, they may then update the 'enabled' field in the 'scheduled_runs.json' file to reflect the 'false' setting.
+  - Users are provided with two options in terms of running the script. They could either run the script once: 'Run.py' or keep it running continuously: 'Start.py'; in the event that they wish to stop running the script, they may then run the following script: 'Stop.py' and should they wish to disable the script completely, they may then update the 'enabled' field in the 'scheduled_runs.json' file to reflect the 'false' setting.
 """
 
 8. Why aren't series and anime not yet supported?
-  - Although there exists a scraper for series, it has proven quite rather difficult when filtering for the necessary qualities in mind and more than that, 'Real-Debrid' has imposed a bandwith limit and since this type of media tends to have a lot of data, it leads to an increase in the time taken to run the script and not only that, but said bandwith limit is bound to be hit sometime soon so it's best that only movies are made the main focus as of now.
-  - There is support for anime, however, that is only extended for movies and not tv shows and with there being little to no endpoints for such media it makes it difficult to find any shows and any websites that house such media are hard to scrape as they either have preventetive measures or they offer multiple entries for one type of media thus making scraping and filtering more difficult.
+  - Although there exists a scraper for series, it has proven quite rather difficult when filtering for the necessary qualities in mind and more than that, 'Real-Debrid' has imposed a bandwidth limit and since this type of media tends to have a lot of data, it leads to an increase in the time taken to run the script and not only that, but said bandwidth limit is bound to be hit sometime soon so it's best that only movies are made the main focus as of now.
+  - There is support for anime, however, that is only extended for movies and not tv shows and with there being little to no endpoints for such media it makes it difficult to find any shows and any websites that house such media are hard to scrape as they either have preventative measures or they offer multiple entries for one type of media thus making scraping and filtering more difficult.
 
 9. How do I contribute to the project?
   - The 'Real Debrid Media Manager' project is open-source and licensed underneath the 'MIT License' so users may feel free to edit and contribute to the project so long as the proper and required credit is given to the original creator of said project as well as the original license, copyright notice and permissions notice, is included.
-  - Users may refrence the 'Contributing' section on the projects main 'GitHub' page to figure out just how to contibute to the project. 
+  - Users may reference the 'Contributing' section on the projects main 'GitHub' page to figure out just how to contribute to the project. 
+
+10. Why is media that I have not added to my 'Watchlist' nor my 'Favourites' being added to my 'Real-Debrid' account or not being added at all even though I have it in my lists?
+  - First and foremost, the user should make sure that they indeed did not add the media in question to either of their lists, purposefully or accidentally, and that no one else has access to their account.
+  - Secondly, in some rare cases, 'Trakt' and the torrent provider might have the media in question listed under different years. Take for example: 'Trakt' might have the movie 'A' as being released in 2024 whilst the torrent provider might have it as being released in 2023 and as such, this can lead to the media not being processed. This issue shouldn't impede the scripts ability to retrieve the correct magnet, though.
+  - Thirdly, the script does aim to accurately capture a media's name however in cases where media is still yet to be released, be it an original, remake or sequel, a keyword might then lead to a torrent with said kewyord being pulled from the torrent providers site, take for example: 'Trakt' might have movie 'A BC' releasing in two months time but the torrent provider might have movie 'A BC D' already listed on their site and as such, the latter will be pulled and processed due to the keyword 'A BC'.
+  - Lastly, the third point should be resolved the moment the media in question is released and a torrent is made available and in the context of the third point, well, as stated, the script should still hopefully be able to pull and process the correct and necessary link.
 
 TUTORIAL_CONTENT = """
 Tutorial Section:
@@ -65,14 +71,14 @@ Tutorial Section:
    - Install the 'Initialize.py' script and run it in your terminal, alternatively, the necessary folders may be downloaded from '[https://github.com/H1ddenShadow/Real-Debrid-Media-Manager.git](https://github.com/H1ddenShadow/Real-Debrid-Media-Manager.git)' and the 'Setup.py' script, found in the 'Scripts' folder, should be ran; it's highly suggested that users make use of the former script whilst those who wish to make use of the latter option must rename the unzipped folder to 'Real Debrid Media Manager'.
 
 2. Cloning a Repository.
-   - Select the 'Clone Repository' option after running the script and in the now opened window, select your preferred directoty.
+   - Select the 'Clone Repository' option after running the script and in the now opened window, select your preferred directory.
    - Wait for the cloning process to complete; the relevant progress will be shown on-screen.
 
 Note: Although a 'renaming' feature is present, it's required that there exists no other folder which shares the same name as 'Real Debrid Media Manager'.
 
 3. Retrieving a users Api Keys.
    - Trakt:
-	- Users must create a 'Trakt' account, if not already existent, wherafter, they should head to the 'Settings' tab which can be found under their name/profile tab.
+	- Users must create a 'Trakt' account, if not already existent, whereafter, they should head to the 'Settings' tab which can be found under their name/profile tab.
 	- There, they should then find a 'Your API Apps' tab and select the 'New Application' option.
 	- In the 'Name' field, a name of the users choosing must be used, whereafter, they must paste the second uri, 'urn:ietf:wg:oauth:2.0:oob', in the 'Redirect uri' field and lastly check both the available options underneath the 'Permissions' field.
 	- Clicking on the 'Save App' button should then allow users to create the app in question.
@@ -85,15 +91,15 @@ Note: Although a 'renaming' feature is present, it's required that there exists 
 	- Users should then head to the respective API page to retrieve their API Key.
 
    - TMDB:
-	- A 'TMDB' account is optional but will be esential for logging purposes.
-	- For convience, users may find their API Key by heading to 'TMDB's' documentaion page and selecting 'Get API Key'.
+	- A 'TMDB' account is optional but will be essential for logging purposes.
+	- For convenience, users may find their API Key by heading to 'TMDB's' documentation page and selecting 'Get API Key'.
 
 4. Setting up the program.
    - Users may run the 'Setup.py' script which may be found in the 'Scripts' folder.
-   - In the requested fields, users should then paste in the respecive keys; being their: 'Client ID', 'Client Secret', 'Authorization Code', 'Real-Debrid API Key' and 'TMDB API Key'. 
-   - Lastly, users may then run the 'Run.py' script to run the script once or the 'Start.py' script to begin continous running.
+   - In the requested fields, users should then paste in the respective keys; being their: 'Client ID', 'Client Secret', 'Authorization Code', 'Real-Debrid API Key' and 'TMDB API Key'. 
+   - Lastly, users may then run the 'Run.py' script to run the script once or the 'Start.py' script to begin continuous running.
 
-Note: The default values will ensure that the script is ran in the early morning, afternoon and evening: 08:00, 12:00 and 18:00, respectively before running off and starting back up the following day. Any changes that the user hopes to make may be made to the 'Schedule.py' file before either the 'Run.py' or 'Start.py' scripts are ran, although it's highly suggested that the former script is ran to create the necessary file with the default values, whereafter users may then feel free to edit the times in said file. The 'Stop.py' should be ran before any changes may be made to the file, and of course, this being if the user has opted to run the latter script (Start.py); this being done to avoid any issues with the script reading or utilizing the wrong times. Lastly, it should be noted that the last available time should have no comma after the qoutations: (""), for example:
+Note: The default values will ensure that the script is ran in the early morning, afternoon and evening: 08:00, 12:00 and 18:00, respectively before running off and starting back up the following day. Any changes that the user hopes to make may be made to the 'Schedule.py' file before either the 'Run.py' or 'Start.py' scripts are ran, although it's highly suggested that the former script is ran to create the necessary file with the default values, whereafter users may then feel free to edit the times in said file. The 'Stop.py' should be ran before any changes may be made to the file, and of course, this being if the user has opted to run the latter script (Start.py); this being done to avoid any issues with the script reading or utilizing the wrong times. Lastly, it should be noted that the last available time should have no comma after the quotations: (""), for example:
 "17:00", 
 "18:00", 
 "20:00"
@@ -105,7 +111,7 @@ would be correct, however:
 would be incorrect and would instead result in there being issues when the data is read.
 
 5. Making Donations.
-   - Donations may be made by running the 'Donations.py' script and then selcting your prefered donation method/service.
+   - Donations may be made by running the 'Donations.py' script and then selecting your preferred donation method/service.
 
 6. Getting Help.
    - For more information, visit the FAQ section or check out the repository's documentation for additional guidance.
