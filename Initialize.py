1cdbc88e16f033298416d8114a548baef86d2261
H1ddenShadow <156337267+H1ddenShadow@users.noreply.github.com>
2024-10-07 19:26:02 +0200

    Initialize.py

--- Initialize.py
+++ Initialize.py
@@ -4,7 +4,6 @@ import sys
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 import git
-import shutil
 import threading
 import ttkbootstrap as tb
 
@@ -40,32 +39,32 @@ FAQ Section:
    - Visit the GitHub repository's issues section or contact support.
 
 4. How do I retrieve my Api Keys?
-   - Users who wish to retrieve their api keys may refrence the respective services' guide on how to retrieve your api keys.
+   - Users who wish to retrieve their api keys may reference the respective services' guide on how to retrieve your api keys.
    - Once retrieved, users may run the 'Setup.py' script to save said keys.
 
 5. How does this script work?
    - The script works by utilizing various endpoints to connect to a users 'Trakt' account, retrieve their lists data: 'Watchlist' and 'Favourites', and fetch the magnet counterparts of said media before adding it to their 'Real-Debrid' account.
 
 6. Why am I facing errors with the 'Scheduled Runs' times as well as the relevant time fields not being updated?
-  - The times present in the 'Schedule.py' script are merely default vaules and adjusting the values from the script itself, after already having run said script, will lead to the changes not being reflected unless the user: deletes the created 'scheduled_runs.json' file and stops the script. Users who wish to make changes to the times are suggested to either do so before running the respective script, initially (the first time) or by running the 'Stop.py' script and then editing the times in the 'scheduled_runs.json' file, which can be found in the 'Scheduled Runs' folder.
+   - The times present in the 'Schedule.py' script are merely default values and adjusting the values from the script itself, after already having run said script, will lead to the changes not being reflected unless the user: deletes the created 'scheduled_runs.json' file and stops the script. Users who wish to make changes to the times are suggested to either do so before running the respective script, initially (the first time) or by running the 'Stop.py' script and then editing the times in the 'scheduled_runs.json' file, which can be found in the 'Scheduled Runs' folder.
 
 7. How do I run or start the script?
-  - Users are provided with two options in terms of running the script. They could either run the script once: 'Run.py' or keep it running continuously: 'Start.py'; in the event that they wish to stop running the script, they may then run the following script: 'Stop.py' and should they wish to disable the script completely, they may then update the 'enabled' field in the 'scheduled_runs.json' file to reflect the 'false' setting.
-"""
+   - Users are provided with two options in terms of running the script. They could either run the script once: 'Run.py' or keep it running continuously: 'Start.py'; in the event that they wish to stop running the script, they may then run the following script: 'Stop.py' and should they wish to disable the script completely, they may then update the 'enabled' field in the 'scheduled_runs.json' file to reflect the 'false' setting.
 
 8. Why aren't series and anime not yet supported?
-  - Although there exists a scraper for series, it has proven quite rather difficult when filtering for the necessary qualities in mind and more than that, 'Real-Debrid' has imposed a bandwidth limit and since this type of media tends to have a lot of data, it leads to an increase in the time taken to run the script and not only that, but said bandwidth limit is bound to be hit sometime soon so it's best that only movies are made the main focus as of now.
-  - There is support for anime, however, that is only extended for movies and not tv shows and with there being little to no endpoints for such media it makes it difficult to find any shows and any websites that house such media are hard to scrape as they either have preventative measures or they offer multiple entries for one type of media thus making scraping and filtering more difficult.
+   - Although there exists a scraper for series, it has proven quite rather difficult when filtering for the necessary qualities in mind and more than that, 'Real-Debrid' has imposed a bandwidth limit and since this type of media tends to have a lot of data, it leads to an increase in the time taken to run the script and not only that, but said bandwidth limit is bound to be hit sometime soon so it's best that only movies are made the main focus as of now.
+   - There is support for anime, however, that is only extended for movies and not tv shows and with there being little to no endpoints for such media it makes it difficult to find any shows and any websites that house such media are hard to scrape as they either have preventative measures or they offer multiple entries for one type of media thus making scraping and filtering more difficult.
 
 9. How do I contribute to the project?
-  - The 'Real Debrid Media Manager' project is open-source and licensed underneath the 'MIT License' so users may feel free to edit and contribute to the project so long as the proper and required credit is given to the original creator of said project as well as the original license, copyright notice and permissions notice, is included.
-  - Users may reference the 'Contributing' section on the projects main 'GitHub' page to figure out just how to contribute to the project. 
+   - The 'Real Debrid Media Manager' project is open-source and licensed underneath the 'MIT License' so users may feel free to edit and contribute to the project so long as the proper and required credit is given to the original creator of said project as well as the original license, copyright notice and permissions notice, is included.
+   - Users may reference the 'Contributing' section on the projects main 'GitHub' page to figure out just how to contribute to the project. 
 
 10. Why is media that I have not added to my 'Watchlist' nor my 'Favourites' being added to my 'Real-Debrid' account or not being added at all even though I have it in my lists?
-  - First and foremost, the user should make sure that they indeed did not add the media in question to either of their lists, purposefully or accidentally, and that no one else has access to their account.
-  - Secondly, in some rare cases, 'Trakt' and the torrent provider might have the media in question listed under different years. Take for example: 'Trakt' might have the movie 'A' as being released in 2024 whilst the torrent provider might have it as being released in 2023 and as such, this can lead to the media not being processed. This issue shouldn't impede the scripts ability to retrieve the correct magnet, though.
-  - Thirdly, the script does aim to accurately capture a media's name, however, in cases where the media in question is still to be released, be it an original, remake or sequel or it's just not on the torrent providers site, then a keyword might then lead to a torrent with said word being pulled from the torrent providers site, take for example: 'Trakt' might have movie 'A BC' releasing in two months time but the torrent provider might have movie 'A BC D' already listed on their site and as such, the latter will be pulled and processed due to the keyword 'A BC' and in other cases, movie 'A BC' might have already been released but not available on the site so the latter movie is then pulled and processed.
-  - Lastly, the third point should be resolved the moment the media in question is released and a torrent is made available and if it has already been released then users need only wait for its torrent to be made available and in the context of the third point, well....as stated, the script should still hopefully be able to pull and process the correct and necessary link.
+   - First and foremost, the user should make sure that they indeed did not add the media in question to either of their lists, purposefully or accidentally, and that no one else has access to their account.
+   - Secondly, in some rare cases, 'Trakt' and the torrent provider might have the media in question listed under different years. Take for example: 'Trakt' might have the movie 'A' as being released in 2024 whilst the torrent provider might have it as being released in 2023 and as such, this can lead to the media not being processed. This issue shouldn't impede the scripts ability to retrieve the correct magnet, though.
+   - Thirdly, the script does aim to accurately capture a media's name, however, in cases where the media in question is still to be released, be it an original, remake or sequel or it's just not on the torrent providers site, then a keyword might then lead to a torrent with said word being pulled from the torrent providers site, take for example: 'Trakt' might have movie 'A BC' releasing in two months time but the torrent provider might have movie 'A BC D' already listed on their site and as such, the latter will be pulled and processed due to the keyword 'A BC' and in other cases, movie 'A BC' might have already been released but not available on the site so the latter movie is then pulled and processed.
+   - Lastly, the third point should be resolved the moment the media in question is released and a torrent is made available and if it has already been released then users need only wait for its torrent to be made available and in the context of the third point, well....as stated, the script should still hopefully be able to pull and process the correct and necessary link.
+"""
 
 TUTORIAL_CONTENT = """
 Tutorial Section:
@@ -80,21 +79,21 @@ Note: Although a 'renaming' feature is present, it's required that there exists 
 
 3. Retrieving a users Api Keys.
    - Trakt:
-	- Users must create a 'Trakt' account, if not already existent, whereafter, they should head to the 'Settings' tab which can be found under their name/profile tab.
-	- There, they should then find a 'Your API Apps' tab and select the 'New Application' option.
-	- In the 'Name' field, a name of the users choosing must be used, whereafter, they must paste the second uri, 'urn:ietf:wg:oauth:2.0:oob', in the 'Redirect uri' field and lastly check both the available options underneath the 'Permissions' field.
-	- Clicking on the 'Save App' button should then allow users to create the app in question.
-	- Users are to then note the 'Client ID' and 'Secret' of said app.
+     - Users must create a 'Trakt' account, if not already existent, whereafter, they should head to the 'Settings' tab which can be found under their name/profile tab.
+     - There, they should then find a 'Your API Apps' tab and select the 'New Application' option.
+     - In the 'Name' field, a name of the users choosing must be used, whereafter, they must paste the second uri, 'urn:ietf:wg:oauth:2.0:oob', in the 'Redirect uri' field and lastly check both the available options underneath the 'Permissions' field.
+     - Clicking on the 'Save App' button should then allow users to create the app in question.
+     - Users are to then note the 'Client ID' and 'Secret' of said app.
    - Authorization Code:
-	- The 'Trakt Authorization Code' may be retrieved by clicking the 'Authorize' button under the 'Redirect URI:' field and should be noted as it'll be used later on in the program. 
+     - The 'Trakt Authorization Code' may be retrieved by clicking the 'Authorize' button under the 'Redirect URI:' field and should be noted as it'll be used later on in the program. 
 
    - Real-Debrid:
-	- A 'Real-Debrid' account will have to be created/existent with an active subscription as well as a current session (logged in).
-	- Users should then head to the respective API page to retrieve their API Key.
+     - A 'Real-Debrid' account will have to be created/existent with an active subscription as well as a current session (logged in).
+     - Users should then head to the respective API page to retrieve their API Key.
 
    - TMDB:
-	- A 'TMDB' account is optional but will be essential for logging purposes.
-	- For convenience, users may find their API Key by heading to 'TMDB's' documentation page and selecting 'Get API Key'.
+     - A 'TMDB' account is optional but will be essential for logging purposes.
+     - For convenience, users may find their API Key by heading to 'TMDB's' documentation page and selecting 'Get API Key'.
 
 4. Setting up the program.
    - Users may run the 'Setup.py' script which may be found in the 'Scripts' folder.
@@ -121,21 +120,25 @@ would be incorrect and would instead result in there being issues when the data 
 
 # URLs
 REPO_URL = 'https://github.com/H1ddenShadow/Real-Debrid-Media-Manager.git'
-FOLDER_URL = 'https://github.com/H1ddenShadow/Real-Debrid-Media-Manager/raw/main/'
 
 def scroll_up(event, canvas):
+    """Scroll the canvas up."""
     canvas.yview_scroll(-1, "units")
 
 def scroll_down(event, canvas):
+    """Scroll the canvas down."""
     canvas.yview_scroll(1, "units")
 
 def scroll_to_top(event, canvas):
+    """Scroll the canvas to the top."""
     canvas.yview_moveto(0)
 
 def scroll_to_bottom(event, canvas):
+    """Scroll the canvas to the bottom."""
     canvas.yview_moveto(1)
 
 def bind_keys(window, canvas):
+    """Bind keys for scrolling the canvas."""
     window.bind_all('<Up>', lambda event: scroll_up(event, canvas))
     window.bind_all('<Down>', lambda event: scroll_down(event, canvas))
     window.bind_all('<w>', lambda event: scroll_up(event, canvas))
@@ -145,7 +148,11 @@ def bind_keys(window, canvas):
     window.bind_all('<a>', lambda event: scroll_to_top(event, canvas))
     window.bind_all('<d>', lambda event: scroll_to_bottom(event, canvas))
 
+# Bind mouse wheel scrolling
+    window.bind_all('<MouseWheel>', lambda event: canvas.yview_scroll(int(-1*(event.delta/120)), "units"))  # Windows
+
 def show_faq():
+    """Display the FAQ content in a new window."""
     faq_window = tb.Toplevel(root)
     faq_window.title("FAQ")
     faq_window.attributes('-fullscreen', True)  # Open in full-screen
@@ -187,6 +194,7 @@ def show_faq():
     bind_keys(faq_window, canvas)
 
 def show_tutorial():
+    """Display the tutorial content in a new window."""
     tutorial_window = tb.Toplevel(root)
     tutorial_window.title("Tutorial")
     tutorial_window.attributes('-fullscreen', True)  # Open in full-screen
@@ -228,6 +236,7 @@ def show_tutorial():
     bind_keys(tutorial_window, canvas)
 
 def clone_repository(progress_var):
+    """Clone the GitHub repository to a selected directory."""
     repo_path = filedialog.askdirectory()
     if repo_path:
         # Note the name of the current directory
@@ -273,6 +282,7 @@ def clone_repository(progress_var):
             progress_bar.pack_forget()  # Hide the progress bar after completion
 
 def progress_callback(progress_var):
+    """Update the progress bar during repository cloning."""
     def update(op_code, cur_count, max_count=None, message=''):
         if max_count:
             progress = (cur_count / max_count) * 100
@@ -281,9 +291,11 @@ def progress_callback(progress_var):
     return update
 
 def clone_repository_thread():
+    """Run the clone repository function in a separate thread."""
     threading.Thread(target=clone_repository, args=(progress_var,)).start()
 
 def main_menu():
+    """Display the main menu with options."""
     for widget in root.winfo_children():
         widget.destroy()
     
